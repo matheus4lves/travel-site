128c0027e1f59a3a39128255704a64192c7d08b7
Matheus Alves <matheusalves.bmd@gmail.com>
2021-02-27 19:49:38 -0300

    Setup Hot Module Replacement

--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,18 @@ module.exports = {
     entry: './app/assets/scripts/App.js',
     output: {
         filename: 'bundled.js',
-        path: path.resolve(__dirname, 'app')
+        path: path.resolve(__dirname, 'app'),
+    },
+    devServer: {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0',
     },
     mode: 'development',
-    watch: true,
     module: {
         rules: [
             {
